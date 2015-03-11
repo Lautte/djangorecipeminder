b41efaf7672d03c685176b666df48e4529267454
Lautte <laura.hurd.arthur@gmail.com>
2015-03-10 22:29:29 -0500

    first commit

---
+++ recipes/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render, HttpResponse, redirect
+from recipes.models import Recipe
+from recipes.forms import RecipeForm
+
+# Create your views here.
+def recipe_list(request):
+	#return HttpResponse("Thiss will be a list of recipes.")
+	recipelist = Recipe.objects.all()
+	context = {'recipelist' : recipelist}
+	return render(request, 'recipelist.html', context)	
+	names = '<li>'.join([r.name for r in recipelist])
+	output = '<ul><li>{0}</ul>'.format(names)	
+	return HttpResponse(output)
+def recipe(request, recipe_id):
+	try:
+		recipe = Recipe.objects.get(id = recipe_id)
+	except Recipe.DoesNotExist:
+		raise Http404
+	context = {'recipe' : recipe}
+	return render(request, 'recipe.html', context)
+
+def addrecipe(request):
+	if request.method == 'POST':
+		form = RecipeForm(request.POST)
+		if form.is_valid():
+			data = form.cleaned_data
+			r = Recipe()
+			r.name = data['name']
+			r.servings = data['servings']
+			r.ingredients = data['ingredients']
+			r.instructions = data['instructions']
+			r.save()
+			return redirect(recipe_list)
+	else:
+		form = RecipeForm()
+	return render(request, 'addrecipe.html', { 'form': form})
